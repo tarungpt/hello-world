39e50ac839e5f72d8288fe12d25212792a854d66
Varun Gupta <varungpt772@gmail.com>
2017-12-05 03:34:01 +0530

    fourth commit

--- app/src/main/java/www/ltbx/in/helloworld/MainActivity.java
+++ app/src/main/java/www/ltbx/in/helloworld/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        some random comment, playing with git
+//        merge in master branch.
     }
 }
