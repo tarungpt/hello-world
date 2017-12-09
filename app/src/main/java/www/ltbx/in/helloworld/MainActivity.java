fc624009b55cf9da4695687de6a77d3a2d3700ff
Varun Gupta <varungpt772@gmail.com>
2017-12-09 22:55:53 +0530

    counter finished

--- app/src/main/java/www/ltbx/in/helloworld/MainActivity.java
+++ app/src/main/java/www/ltbx/in/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package www.ltbx.in.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,9 +11,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private TextView textView;
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         editText.setText("");
         textView.setText("");
         textView.setMovementMethod(new ScrollingMovementMethod());
+        Log.d(TAG, "onCreate: out");
     }
 
     public void logName(View view) {
@@ -28,4 +31,61 @@ public class MainActivity extends AppCompatActivity {
         editText.setText("");
 
     }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
+    }
+
 }
